56e5f5fae793a552e46ce136ed622da00f9c9bc3
Tony.Jaa <1291813139@qq.com>
2017-06-12 09:59:10 +0800

    come on

---
+++ src/main/java/demo/servlet/UserAction.java
@@ -0,0 +1,37 @@
+package demo.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by zhenya.1291813139.com
+ * on 2017/6/12.
+ * Java EE_1702.
+ */
+@WebServlet(urlPatterns = "/user")
+public class UserAction extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String action = req.getParameter("action");
+        System.out.println(action);
+        if (action.equals("register")) {
+            // TODO: 6/12/17 register
+        } else if (action.equals("login")) {
+            // TODO: 6/12/17 login
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String action = req.getParameter("action");
+        if (action.equals("logout")) {
+            req.getSession().invalidate();
+            resp.sendRedirect("index.jsp");
+        } else {
+        }
+    }
+}
